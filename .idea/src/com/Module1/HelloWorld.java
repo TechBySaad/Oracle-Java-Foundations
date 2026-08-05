e441ab00e698b2b79156b2b7d6b15a0b823fd306
SaadDev <saad26dev@gmail.com>
2026-08-06 01:26:20 +0530

    Explanation of main method

---
+++ .idea/src/com/Module1/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.Module1;
+
+public class HelloWorld { // class is the main unit of code in java
+    public static void main(String[] args) { // this is the main method, also the entry point of the java application
+        System.out.println("Hello World!"); // this is the print statement which give the output Hello World
+    }
+}
