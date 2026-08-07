a102c524e04bccafc2c12c60f26f0fcb14b49465
SaadDev <saad26dev@gmail.com>
2026-08-07 19:36:28 +0530

    Console reading

--- .idea/src/com/Module1/HelloWorld.java
+++ .idea/src/com/Module1/HelloWorld.java
@@ -2,6 +2,6 @@ package com.Module1;
 
 public class HelloWorld { // class is the main unit of code in java
     public static void main(String[] args) { // this is the main method, also the entry point of the java application
-        System.out.println("Hello World!"); // this is the print statement which give the output Hello World
+        System.out.println("Hello "+args[0]); // this is the print statement which give the output Hello World
     }
 }
