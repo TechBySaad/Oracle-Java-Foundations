ae62b62162b6d139b4beea9056305dc7f5110e2d
SaadDev <saad26dev@gmail.com>
2026-08-15 01:00:49 +0530

    Arithmetic and Assignment operators

---
+++ .idea/src/com/Module3/ArithmeticAssignment.java
@@ -0,0 +1,15 @@
+package com.Module3;
+
+public class ArithmeticAssignment {
+    public static void main(String[] args) {
+        int age = 22; // Assignment
+        double weight = 77.1; // Assignment
+
+        int a = 6 + 5; // Arithmetic addition
+        int b = 5 - 2; // Arithmetic subtraction
+        int c = 4 * 5; // Arithmetic multiplication
+        double d = 8 / 4; // Arithmetic division
+        int e = 7 % 2; // Arithmetic modulus
+
+    }
+}
